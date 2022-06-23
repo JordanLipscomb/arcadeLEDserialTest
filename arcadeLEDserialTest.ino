00d627c3daf30d6dbcf821874d72193c49f9b922
Jordan L <jordandl89@knights.ucf.edu>
2022-06-23 17:21:01 -0400

    Added Ignore
    Added 4 potential Unity file ignores.

--- arcadeLEDserialTest.ino
+++ arcadeLEDserialTest.ino
@@ -2,12 +2,25 @@
 #define PIN 6
 
 void setup() {
+  Serial.begin(19200);
+  Serial.setTimeout(500);
   pinMode(PIN, OUTPUT);
   digitalWrite(PIN, LOW);
 
 }
 
 void loop() {
-  
+  if (Serial.available() > 0){
+    String str = Serial.readString();
+    Serial.println(str);
+    
+    if (str == "LEDon"){
+      digitalWrite(PIN, HIGH);
+    }
 
+    if (str == "LEDoff"){
+      digitalWrite(PIN, LOW);
+    }
+    
+  }
 }
