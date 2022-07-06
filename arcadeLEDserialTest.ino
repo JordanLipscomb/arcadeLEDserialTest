469d01f9672c764d79bb29cdadb9607c4ac054a2
Jordan Lipscomb <jordan.lipscomb@ucf.edu>
2022-07-06 16:17:11 -0400

    String Serial
    Change char based switch to string based switch

--- arcadeLEDserialTest.ino
+++ arcadeLEDserialTest.ino
@@ -6,32 +6,55 @@
 
 unsigned long eventRef = 0UL;
 long brightness;
-int aLEDswitch;
-String StrUno;
-String StrDos;
+const unsigned int MAX_MESSAGE_LENGTH = 12;
+String aLEDswitch;
 Adafruit_NeoPixel pixelStrip = Adafruit_NeoPixel(nLEDS,LED_PIN,NEO_GRB+NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
+  aLEDswitch = "rainbow";
   pixelStrip.setBrightness(ledBright);
   pixelStrip.begin();
   pixelStrip.show();
-  aLEDswitch = '1';
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    aLEDswitch = Serial.read();
-    StrUno = "String Read: ";
-    StrDos = StrUno + aLEDswitch ;
-  }
-  Serial.println(StrDos);
-  if (aLEDswitch == '0') {
+  //Reads in strings send to the Arduino over the serial.
+  arcadeSerialBuffer();
+  //Switches LED color patterns based on string received from arcadeSerialBuffer()
+  if (aLEDswitch == "off"){
     lightsOff(1000UL);
   }
-  else {
+  else{
     rainbow(1000UL);
+  }
+}
+
+void arcadeSerialBuffer(){
+  //Check for data in the serial receive buffer
+  while (Serial.available() > 0){
+    //Create an array to hold incoming message and sets the byte position in the serial buffer
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos = 0;
+    //Read next byte in the serial recieve buffer
+    char inByte = Serial.read();
+    //Check if incoming message is not a terminating character
+    if(inByte != '\n' && (message_pos <= MAX_MESSAGE_LENGTH - 1)){
+      //Add the incoming byte to our message
+      message[message_pos] = inByte;
+      message_pos++;
+    }
+    //Full message received
+    else{
+      //Add null character to message addess message to useable string
+      message[message_pos] = '\0';
+      aLEDswitch = message;
+      //Reset for the next message
+      Serial.end();
+      Serial.begin(9600);
+      message_pos = 0;
+    }
   }
 }
 
@@ -42,7 +65,7 @@ void lightsOff(unsigned long defltInterval){
       pixelStrip.setPixelColor(i,0,0,0);
       pixelStrip.show();
     }
-    Serial.println("Off");
+    Serial.println("Lights are off");
   }
   else {
     eventRef = millis();
@@ -57,7 +80,7 @@ void rainbow(unsigned long animInterval){
       pixelStrip.setPixelColor(i,0,255,0);
       pixelStrip.show();
     }
-    Serial.println("Red");
+    Serial.println("Lights are rainbow");
     /*
     uint16_t i, j;
     for(j=0; j<256*cycles; j++) {
